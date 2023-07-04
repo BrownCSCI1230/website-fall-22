31b09cb0b7ff002e3ffd3f7c75dc753a6d7af87d
Zack Cheng <zack_cheng@brown.edu>
2023-07-04 13:52:06 +0800

    Updated build | Jul 04, 01:51PM

---
+++ assets/pages/labs/index.page.8e8cdc8b.js
@@ -0,0 +1,16 @@
+import{j as e}from"../../chunk-4cdb8c1a.js";const n={title:"Labs",hideTOC:!0};function r(l){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p"},l.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(s.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#labs",children:"Labs"})})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"labs",children:e.exports.jsx(s.a,{href:"#labs",children:"Labs"})}),`
+`,e.exports.jsx(s.p,{children:"CS 1230 has 11 labs. Each is designed to take 1-2 h."}),`
+`,e.exports.jsxs(s.ol,{children:[`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab1",children:"Setup & C++"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab2",children:"Pencils"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab3",children:"Convolution"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab4",children:"Transforms"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab5",children:"Parsing"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab6",children:"Light"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab7",children:"Terrain"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab8",children:"Trimeshes"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab9",children:"VBOs & VAOs"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab10",children:"Shaders"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/website-fall-22/labs/lab11",children:"Textures & FBOs"})}),`
+`]})]})]})}function a(l={}){const{wrapper:s}=l.components||{};return s?e.exports.jsx(s,Object.assign({},l,{children:e.exports.jsx(r,l)})):r(l)}export{a as default,n as documentProps};
---
+++ assets/pages/projects/index.page.57419c91.js
@@ -0,0 +1,28 @@
+import{j as e}from"../../chunk-4cdb8c1a.js";const l={title:"Projects",hideTOC:!0};function t(r){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",ul:"ul",strong:"strong"},r.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(s.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#projects",children:"Projects"})})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"projects",children:e.exports.jsx(s.a,{href:"#projects",children:"Projects"})}),`
+`,e.exports.jsx(s.p,{children:"CS 1230 has 6 projects + 1 final project. Each spans 2 weeks."}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Raster"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:["Project 1: ",e.exports.jsx(s.a,{href:"/website-fall-22/projects/2D/1",children:"Brush"})]}),`
+`,e.exports.jsxs(s.li,{children:["Project 2: ",e.exports.jsx(s.a,{href:"/website-fall-22/projects/2D/2",children:"Filter"})]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Ray"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:["Project 3: ",e.exports.jsx(s.a,{href:"/website-fall-22/projects/ray/1",children:"Intersect"})]}),`
+`,e.exports.jsxs(s.li,{children:["Project 4: ",e.exports.jsx(s.a,{href:"/website-fall-22/projects/ray/2",children:"Illuminate"})]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Realtime"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:["Project 5: ",e.exports.jsx(s.a,{href:"/website-fall-22/projects/realtime/1",children:"Lights, Camera"})]}),`
+`,e.exports.jsxs(s.li,{children:["Project 6: ",e.exports.jsx(s.a,{href:"/website-fall-22/projects/realtime/2",children:"Action!"})]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Final"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:["Project 7: ",e.exports.jsx(s.a,{href:"/website-fall-22/projects/final",children:"Final Project"})]}),`
+`]}),`
+`]}),`
+`]})]})]})}function o(r={}){const{wrapper:s}=r.components||{};return s?e.exports.jsx(s,Object.assign({},r,{children:e.exports.jsx(t,r)})):t(r)}export{o as default,l as documentProps};
